ab2350c1433e6c2c0767794ae7ecfa33781abc1f
Carlos Gutierrez <96015610+CharlysHD@users.noreply.github.com>
2022-11-22 03:12:38 -0300

    Add files via upload

---
+++ src/main/java/com/MyPhoneStore/ecommerce/service/IDetalleOrdenService.java
@@ -0,0 +1,8 @@
+package com.MyPhoneStore.ecommerce.service;
+
+import com.curso.ecommerce.model.DetalleOrden;
+
+public interface IDetalleOrdenService {
+	DetalleOrden save (DetalleOrden detalleOrden);
+
+}
